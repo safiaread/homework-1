0e4497045419f91aa802a7edc162db25d73b996a
Safia Read <safia.read@emory.edu>
2024-01-29 12:08:59 -0500

    class

--- submission1/analysis/analysis_draft.r
+++ submission1/analysis/analysis_draft.r
@@ -1,6 +1,6 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
-data <- readRDS("/Users/safiaread/Desktop/homework_1/data/output/full_ma_data.rds")
+data <- readRDS("data/output/full_ma_data.rds")
 data <- data %>%
 subset(year > 2009)
 # n_obs(data)
@@ -13,6 +13,7 @@ summary(data)
 mini_data <- data[1:50,]
 summary(mini_data)
 plan_mdata <- subset(mini_data, select = c(plan_type, year, snp, eghp, plan_name))
+count(unique(data$plan_type))
 
 
 library(tidyverse)
@@ -44,3 +45,6 @@ head(service)
 head(q4)
 q4 %>%
   inner_join(service, by=c("contractid", "fips", "year"))
+
+#rm(list = c("remove large data sets"))
+#save.image("submission/hw1_workspace.Rdata"))
